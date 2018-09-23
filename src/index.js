d4bc265cea1938630e4990bc952fb952f65877be
Nat Sharpe <sharpe1890@gmail.com>
2018-09-23 12:15:36 -0400

    Reducer takes two types of actions and either decs or incs the state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import { createStore } from "redux";
 
-const reducer = () => {
-}
+const reducer = (state, action) => {
+    console.log(state)
+    console.log(action)
+
+    if (action.type === "BIGGER") {
+        return state + action.payload;
+    } 
+    if (action.type === "LESSER") { 
+        return state - action.payload
+    }
+    return state
+};
 
 const store = createStore(reducer, 0);
+
+console.log(store.getState());
 
 store.subscribe(() => {
     console.log("Store change", store.getState())
 });
 
-store.dispatch({
-    type: "BIGGER",
-    payload: 100
-})
+store.dispatch({type: "BIGGER", payload: 10}) 
+store.dispatch({type: "BIGGER", payload: 200}) 
+store.dispatch({type: "LESSER", payload: 30}) 
+store.dispatch({type: "BIGGER", payload: 1}) 
+store.dispatch({type: "BIGGER", payload: 600}) 
+store.dispatch({type: "LESSER", payload: 5}) 
+
